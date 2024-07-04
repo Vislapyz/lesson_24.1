2e88b921eeae6cad63ff77a898fa3e6e0aafcc08
Mihail <vislapyz@mail.ru>
2024-07-04 10:46:19 +0300

    Новая модель пробег и меграция
    Signed-off-by: Mihail <vislapyz@mail.ru>

--- vehicle/models.py
+++ vehicle/models.py
@@ -21,5 +21,20 @@ class Moto(models.Model):
         return f"{self.title}"
 
     class Meta:
-        verbose_name = "мотоцикл"
+        verbose_name = "Мотоцикл"
         verbose_name_plural = "Мотоциклы"
+
+
+class Milage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name="Машина")
+    moto = models.ForeignKey(Moto, on_delete=models.CASCADE, verbose_name="Мотоцикл")
+    milage = models.PositiveIntegerField(verbose_name="Пробег")
+    year = models.PositiveSmallIntegerField(verbose_name="Год регистрации")
+
+    def __str__(self):
+        return f"{self.moto if self.moto else self.car} - {self.year}"
+
+    class Meta:
+        verbose_name = "Пробег"
+        verbose_name_plural = "Пробег"
+        ordering = ("-year",)
