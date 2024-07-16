2b261e87b6eedf10faf92f5f519ea3e1ba3d826a
Mihail <vislapyz@mail.ru>
2024-07-16 20:12:59 +0300

    25.2  Валидаторы, пагинация и тесты
    Signed-off-by: Mihail <vislapyz@mail.ru>

--- vehicle/validators.py
+++ vehicle/validators.py
@@ -9,7 +9,7 @@ class TitleValidator:
         self.feild = feild
 
     def __call__(self, value):
-        reg = re.compile('^,[a-zA-Z0-9\,\-\ ]+s')
+        reg = re.compile('^[a-zA-Z0-9\.\-\ ]+$')
         tmp_val = dict(value).get(self.feild)
         if not bool(reg.match(tmp_val)):
             raise ValidationError("Title is not ok")
